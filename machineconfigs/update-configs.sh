24940463d0c4b17a0c9e49a40c010366e0b00f02
Mathias Pius <contact@pius.io>
2024-05-14 12:06:34 +0200

    Update machine configs

--- machineconfigs/update-configs.sh
+++ machineconfigs/update-configs.sh
@@ -95,7 +95,8 @@ function update_config() {
   
   # Fetch the node's machineconfig, extracting only the spec itself, and 
   # storing it in our temp file.
-  talosctl -e "$1" -n "$2"  get machineconfig -o yaml | yq '.spec' > "$TEMP_MACHINECONFIG_FILE"
+  echo "Fetching machineconfig for $2 from $1.."
+  talosctl -e "$1" -n "$2"  get machineconfig v1alpha1 -o yaml | yq '.spec' > "$TEMP_MACHINECONFIG_FILE"
 
   # Invoke sops' edit mode for the target file, causing it to decrypt
   # into a temp file and in turn invoke this script as EDITOR.
