8e2d6575bf98bb35baca8edea2ad39a7f7c13a4e
Mathias Pius <contact@pius.io>
2025-11-03 14:21:37 +0100

    Upgrade to new cluster version

--- tools/entrypoint.sh
+++ tools/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 mkdir /home/user/.talos
-sops -d --input-type=yaml --output-type=yaml talosconfig > /home/user/.talos/config
+sops -d --input-type=yaml --output-type=yaml configs/talosconfig.yaml > /home/user/.talos/config
 
 mkdir /home/user/.kube
-sops -d --input-type=yaml --output-type=yaml kubeconfig > /home/user/.kube/config
+sops -d --input-type=yaml --output-type=yaml configs/kubeconfig.yaml > /home/user/.kube/config
 
 if [ $# -eq 0 ]; then
     bash
